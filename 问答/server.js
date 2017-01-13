67b4d1f778304ce4cd1e8a510a67592b5f0ba3cb
Roy <zuy89@icloud.com>
2017-01-13 15:16:09 +0800

    roy完成了注册功能

--- 问答/server.js
+++ 问答/server.js
@@ -1,13 +1,54 @@
 const express = require('express'),
 
-	  app = express()
+		  bodyParser = require('body-parser'),
+		  
+		  fs = require('fs'),
+
+	    app = express()
 	  
 app.use(express.static('static'))
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 /*
  * 注册接口
  */
 app.post('/user/register',(req,res)=>{
+	
+	var userName = req.body.userName;
+	
+	var filePath = `users/${userName}.txt`;
+	
+	fs.exists(filePath,(isExists)=>{
+		
+		if(isExists){
+			
+			//已经注册
+			res.status(200).json({result:0,msg:'该用户名已经被使用'})
+		}
+		else{
+			
+			var userMsg = {
+				psw:req.body.psw,
+				email:req.body.email,
+				sex:req.body.sex,
+				course:req.body.course
+			}
+			//没有注册
+			fs.appendFile(filePath,JSON.stringify(userMsg),(err)=>{
+				
+				if(err){
+					
+					res.status(500).json({result:0,msg:'服务端出错了'})
+				}
+				else{
+					
+					res.status(200).json({result:1,msg:'注册成功'})
+					
+				}
+			})
+		}
+	})
 	
 })
 app.listen(3000,function(){
--- 问答/static/js/register.js
+++ 问答/static/js/register.js
@@ -1,21 +1,78 @@
-document.forms[0].onsubmit = function(e){
-	
+document.forms[0].onsubmit = function(e) {
+
 	e.preventDefault()
-	
+
 	//获取两个密码框内的内容
 	var allPsw = []
-	
-	$(':password').each(function(){
-		
+
+	$(':password').each(function() {
+
 		allPsw.push($(this).val())
 	})
-	
-	if(allPsw[0] != allPsw[1]){
-		
+
+	if(allPsw[0] != allPsw[1]) {
+
 		//alert('两次密码输入的不一致')
 		//弹出模态框，并设置显示内容
 		$('#myModal').modal('show').find('.modal-body').text('两次密码输入的不一致,请确认后重新提交。')
 	}
+
+	//获取表单数据，并序列化为formdata格式的数据对象
+	var data = new FormData(this)
+
+	console.log(data)
+		//使用jQuery发送请求，假如数据数据是从form表单内获取，则需要使用这种方式获取表单数据。它代表把表单内的数据序列化为js对象。对象内是发送数据，请求体数据格式。
+	data = $(this).serialize()
+
+	console.log(data)
+
+	sendToRgister(data)
+}
+
+/*
+ * 发送注册请求
+ */
+function sendToRgister(data) {
+
+	/*
+	//方法一:使用js的ajax请求
+	var xhr = new XMLHttpRequest()
 	
+	xhr.onreadystatechange = function(){
+		
+		if(xhr.readyState == 4){
+			
+			var response = JSON.parse(xhr.response)
+			
+			alert(response.msg)
+		}
+	}
 	
+	xhr.open('POST','/user/register')
+	
+	xhr.send(data)
+	*/
+
+	//方法二：
+	//使用jQuery的post()发送请求
+	$.post('/user/register', data, function(response, statusText, xhr) {
+
+		if(response.result == 0) {
+
+			//注册失败
+			$('#myModal').modal('show').find('.modal-body').text('注册失败,请稍后重试!')
+		} else {
+			//注册成功
+			$('#myModal').modal('show').find('.modal-body').text('恭喜你,注册成功!')
+
+			//跳转登录页面
+			$('#myModal').on('hidden.bs.modal', function() {
+				
+				//location.href = 'login.html'
+
+			})
+		}
+
+	})
+
 }
